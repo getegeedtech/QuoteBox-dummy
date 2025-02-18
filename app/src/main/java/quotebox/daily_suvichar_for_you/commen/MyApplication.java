0602680d835c646827b4a8aeba054c7d3d7b43bb
getegeedtech <getegeedtech@gmail.com>
2025-02-19 00:10:31 +0530

    Update With Resolve basic error

--- app/src/main/java/quotebox/daily_suvichar_for_you/commen/MyApplication.java
+++ app/src/main/java/quotebox/daily_suvichar_for_you/commen/MyApplication.java
@@ -2,12 +2,11 @@ package quotebox.daily_suvichar_for_you.commen;
 
 import android.app.Application;
 
-import com.google.android.gms.ads.MobileAds;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        MobileAds.initialize(this, initializationStatus -> {});
+
     }
 }
